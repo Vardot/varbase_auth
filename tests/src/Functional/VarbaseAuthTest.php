2127b6c437e4013803040c41dbd929ce52a27d9d
Rajab Natshah <rajabn@gmail.com>
2020-04-20 12:48:13 +0300

    Issue #3128227: Add Automated Testing for the [Varbase Social Single Sign-On] module

---
+++ tests/src/Functional/VarbaseAuthTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\Tests\varbase_auth\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Varbase Auth test.
+ *
+ * @group varbase_auth
+ */
+class VarbaseAuthTest extends BrowserTestBase {
+
+  use StringTranslationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'varbase_auth',
+    'social_auth',
+    'social_auth_facebook',
+    'social_auth_google',
+    'social_auth_linkedin',
+    'social_auth_twitter',
+  ];
+
+  /**
+   * Check Varbase Auth Login page.
+   */
+  public function testCheckVarbaseAuthLoginPage() {
+    $assert_session = $this->assertSession();
+
+    $this->drupalGet('/user/login');
+
+    $assert_session->elementContains('css', '.block.block-social-auth.block-social-auth-login');
+
+  }
+
+  /**
+   * Check Varbase Auth User register.
+   */
+  public function testCheckVarbaseAuthUserRegister() {
+    $assert_session = $this->assertSession();
+
+    $this->drupalGet('/user/register');
+
+    $assert_session->elementContains('css', '.block.block-social-auth.block-social-auth-login');
+
+  }
+
+  /**
+   * Check Varbase Auth User authentication.
+   */
+  public function testCheckVarbaseAuthUserAuthentication() {
+    $assert_session = $this->assertSession();
+
+    $this->drupalGet('/admin/config/social-api/social-auth');
+    $assert_session->pageTextContains('social_auth_facebook');
+    $assert_session->pageTextContains('social_auth_google');
+    $assert_session->pageTextContains('social_auth_linkedin');
+    $assert_session->pageTextContains('social_auth_twitter');
+
+  }
+
+}
